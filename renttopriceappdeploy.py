90f0fbeefecd112a596e234555f0fcb711386934
mmaminghui <95901728+mmaminghui@users.noreply.github.com>
2022-08-08 15:09:54 -0400

    Add files via upload

--- renttopriceappdeploy.py
+++ renttopriceappdeploy.py
@@ -12,13 +12,14 @@ import numpy as np
 import pandas as pd
 
 
-pickle_in = open("renttopricemodel.pkl","rb")
+#pickle_in = open("/Users/minghuima/Desktop/my/analysis1.pkl","rb")
+pickle_in = open("analysis1.pkl","rb")
 renttopricemodel=pickle.load(pickle_in)
 
 def welcome():
     return "Welcome All"
 
-def ratio_prediction(zipcode,longitude,latitude,yearbuilt,livingarea,bathrooms,annualhomeownersinsurance,bedrooms,monthlyhoafee):
+def ratio_prediction(zipcode,longitude,latitude,yearbuilt,livingarea,bathrooms,bedrooms,annualtax,annualinsurance):
     
     """Rent to Price Ratio Prediction Note 
     This is using docstrings for specifications.
@@ -48,18 +49,20 @@ def ratio_prediction(zipcode,longitude,latitude,yearbuilt,livingarea,bathrooms,a
         in: query
         type: number
         required: true
-      - name: annualhomeownersinsurance
-        in: query
-        type: number
-        required: true
       - name: bedrooms
         in: query
         type: number
         required: true
-      - name: monthlyhoafee
+      - name: annualtax
+        in: query
+        type: number
+        required: true        
+      - name: annualinsurance
         in: query
         type: number
         required: true
+
+
       
     responses:
         200:
@@ -67,14 +70,14 @@ def ratio_prediction(zipcode,longitude,latitude,yearbuilt,livingarea,bathrooms,a
         
     """
    
-    prediction=renttopricemodel.predict([[zipcode,longitude,latitude,yearbuilt,livingarea,bathrooms,annualhomeownersinsurance,bedrooms,monthlyhoafee]])
-    print(prediction)
+    prediction=renttopricemodel.predict([[zipcode,longitude,latitude,yearbuilt,livingarea,bathrooms,bedrooms,annualtax,annualinsurance]])*100
+    #print prediction
     return prediction
 
 
 
 def main():
-    st.title("Rent to Price Ratio Prediction App for Rental Property Investors")
+    st.title("Rent to Price Ratio Prediction")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
     <h2 style="color:white;text-align:center;">Does your target house meet the 1% rule? 
@@ -88,17 +91,19 @@ def main():
     yearbuilt = st.text_input("yearbuilt","Type Here")
     livingarea = st.text_input("livingarea","Type Here")
     bathrooms = st.text_input("bathrooms","Type Here")
-    annualhomeownersinsurance = st.text_input("annualhomeownersinsurance","Type Here")
     bedrooms = st.text_input("bedrooms","Type Here")
-    monthlyhoafee = st.text_input("monthlyhoafee","Type Here")
+    annualtax = st.text_input("annualtax","Type Here")
+    annualinsurance = st.text_input("annualinsurance","Type Here")
     result=""
     if st.button("Predict"):
-        result=ratio_prediction(zipcode,longitude,latitude,yearbuilt,livingarea,bathrooms,annualhomeownersinsurance,bedrooms,monthlyhoafee)
-    st.success('The output is {}'.format(result))
+        result=ratio_prediction(zipcode,longitude,latitude,yearbuilt,livingarea,bathrooms,bedrooms,annualtax,annualinsurance)
+    st.success('The rent-to-price ratio is {}%'.format(result))
+
     if st.button("About"):
         st.text("This app is based on the transaction data (2022.1-2022.6) in Baltimore County")
+        st.text("")
         st.text("This app can be customized for your local area")
-        st.text("Built with Streamlit")
+
 
 if __name__=='__main__':
     main()
